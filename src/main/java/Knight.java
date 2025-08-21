53f2cd59ef226f3ef7ff2c3e9ef49f458415ae4b
Yunjeong Lee <44536978+yunjeong-lee@users.noreply.github.com>
2025-08-21 21:07:44 +0800

    Add ability to store

--- src/main/java/Knight.java
+++ src/main/java/Knight.java
@@ -1,18 +1,42 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Knight {
-    private static void print(String input) {
+    // Helper to simplify println
+    public static void print(String input) {
         System.out.println(input);
     }
+    static String line = "__________";
+    static String divideLine = "\t" + line.repeat(6) + "\n";
+
+    // Initialize ArrayList tasks with capacity 100
+    public static ArrayList<String> tasks = new ArrayList<>(100);
+
+    public static void addTask(String taskInput) {
+        tasks.add(taskInput);
+        System.out.println(tasks.toArray().length);
+        print(divideLine + "\t added: " + taskInput + "\n" + divideLine);
+    }
+
+    public static void listTasks() {
+        for (int i = 0; i < tasks.toArray().length; i++) {
+            String index = String.valueOf(i+1);
+            print(index + ". " + tasks.get(i));
+        }
+        print("\n");
+    }
+
     public static void main(String[] args) {
-        String line = "__________";
-        String divideLine = "\t" + line.repeat(6) + "\n";
         print(divideLine + "\t Hello! I'm Knight\n\t What can I do for you?\n" + divideLine);
 
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.next();
         do {
-            print(divideLine + "\t " + userInput + "\n" + divideLine);
+            if (userInput.equals("list")) {
+                listTasks();
+            } else {
+                addTask(userInput);
+            }
             userInput = scanner.next();
         } while (!userInput.equals("bye"));
 
