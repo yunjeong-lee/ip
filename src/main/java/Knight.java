c0f4403a7842986da54b42486c3f7289018353af
Yunjeong Lee <44536978+yunjeong-lee@users.noreply.github.com>
2025-08-21 20:21:01 +0800

    Add renamed Knight.java

---
+++ src/main/java/Knight.java
@@ -0,0 +1,9 @@
+public class Knight {
+    public static void main(String[] args) {
+        String line = "__________";
+        String divideLine = line.repeat(6) + "\n";
+        //String lin = "____________________________________________________________\n";
+        System.out.println(divideLine + " Hello! I'm Knight\n What can I do for you?\n"
+                + divideLine + " Bye. Hope to see you again soon!\n" + divideLine);
+    }
+}
