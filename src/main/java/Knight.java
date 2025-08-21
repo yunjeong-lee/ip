f398bbea5214aa36aca42480878e630dc5dbe9c8
Yunjeong Lee <44536978+yunjeong-lee@users.noreply.github.com>
2025-08-21 20:48:38 +0800

    Improve skeletal version

--- src/main/java/Knight.java
+++ src/main/java/Knight.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Knight {
+    private static void print(String input) {
+        System.out.println(input);
+    }
     public static void main(String[] args) {
         String line = "__________";
-        String divideLine = line.repeat(6) + "\n";
-        //String lin = "____________________________________________________________\n";
-        System.out.println(divideLine + " Hello! I'm Knight\n What can I do for you?\n"
-                + divideLine + " Bye. Hope to see you again soon!\n" + divideLine);
+        String divideLine = "\t" + line.repeat(6) + "\n";
+        print(divideLine + "\t Hello! I'm Knight\n\t What can I do for you?\n" + divideLine);
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.next();
+        do {
+            print(divideLine + "\t " + userInput + "\n" + divideLine);
+            userInput = scanner.next();
+        } while (!userInput.equals("bye"));
+
+        print(divideLine + "\t Bye. Hope to see you again soon!\n" + divideLine);
+        scanner.close();
     }
 }
