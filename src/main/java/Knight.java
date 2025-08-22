75a2a6f2afc5343597b694a69133fe85289ae077
Yunjeong Lee <44536978+yunjeong-lee@users.noreply.github.com>
2025-08-22 12:42:33 +0800

    Add support for diff types of tasks

--- src/main/java/Knight.java
+++ src/main/java/Knight.java
@@ -18,7 +18,8 @@ public class Knight {
     // addTask : add to taskStorage and print the added
     public static void addTask(Task taskInput) {
         taskStorage.add(taskInput);
-        print(divideLine + "\t added: " + taskInput.taskName + "\n" + divideLine);
+        print(divideLine + "\t Got it. I've added this task:\n\t   " + taskInput.toString() + "\n"
+                + "\t Now you have " + taskStorage.toArray().length + " tasks in the list.\n" + divideLine);
     }
 
     // listTasks : list all elements in 'tasks'
@@ -63,10 +64,6 @@ public class Knight {
         return String.join(" ", newInput);
     }
 
-    public static void addDeadline(Deadline dTask) {
-        taskStorage.add(dTask);
-        print(divideLine + "\t Got it. I've added this task:\n\t   " + dTask.toString() + "\n" + divideLine);
-    }
     public static void main(String[] args) {
         print(divideLine + "\t Hello! I'm Knight\n\t What can I do for you?\n" + divideLine);
 
@@ -79,22 +76,55 @@ public class Knight {
             } else if (startsWithString(userInput, "mark")) {
                 int inputIndex = Integer.valueOf(userInput.split(" ")[1]) - 1;
                 markTaskStorageAt(inputIndex);
-            } else if (startsWithString(userInput, "mark")) {
+            } else if (startsWithString(userInput, "unmark")) {
                 int inputIndex = Integer.valueOf(userInput.split(" ")[1]) - 1;
                 unmarkTaskStorageAt(inputIndex);
+            } else if (startsWithString(userInput, "todo")) {
+                String todoTaskDescription = withoutFirst(userInput);
+                Todo todoTask = new Todo(todoTaskDescription);
+                addTask(todoTask);
+            } else if (startsWithString(userInput, "event")) {
+                String withoutFirstString = withoutFirst(userInput);
+                String[] eventArr = withoutFirstString.split(" ");
+                // First find indexes of "\from" and "\to"
+                List<String> eventList = Arrays.asList(eventArr);
+                int indexFrom = eventList.indexOf("/from");
+                int indexTo = eventList.indexOf("/to");
+                String[] descriptionArr = new String[indexFrom];
+                String[] fromArr = new String[indexTo - indexFrom - 1];
+                String[] toArr = new String[eventArr.length - indexTo - 1];
+                System.arraycopy(eventArr, 0, descriptionArr, 0, indexFrom);
+                System.arraycopy(eventArr, indexFrom + 1, fromArr, 0, indexTo - indexFrom - 1);
+                System.arraycopy(eventArr, indexTo + 1, toArr, 0, eventArr.length - indexTo - 1);
+
+                // Put together
+                String description = String.join(" ", descriptionArr);
+                String from = String.join(" ", fromArr);
+                String to = (toArr.length == 1) ? toArr[0] : String.join(" ", toArr);
+//                print("description: " + description + "\nfrom: " + from + "\nto: " + to);
+
+                // Create Event task and add
+                Event eventTask = new Event(description, from, to);
+                addTask(eventTask);
             } else if (startsWithString(userInput, "deadline")) {
                 String withoutFirstString = withoutFirst(userInput);
-
                 String[] deadlinesArr = withoutFirstString.split(" ");
+
                 // First find index of "\by"
                 List<String> deadlinesList = Arrays.asList(deadlinesArr);
                 int indexBy = deadlinesList.indexOf("/by");
                 String[] descriptionArr = new String[indexBy];
+                String[] byArr = new String[deadlinesArr.length - indexBy - 1];
                 System.arraycopy(deadlinesArr, 0, descriptionArr, 0, indexBy);
+                System.arraycopy(deadlinesArr, indexBy + 1, byArr, 0, deadlinesArr.length - indexBy - 1);
 
+                // Put together
                 String description = String.join(" ", descriptionArr);
-                Deadline deadlineTask = new Deadline(description,"some date");
-                addDeadline(deadlineTask);
+                String by = String.join(" ", byArr);
+
+                // Create Deadline task and add
+                Deadline deadlineTask = new Deadline(description, by);
+                addTask(deadlineTask);
             } else {
                 addTask(inputTask); //userInput
             }
